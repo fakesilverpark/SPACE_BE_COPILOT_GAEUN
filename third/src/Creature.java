560287896957a833bb5fc2a89827afd1c1527707
eunbb <fakesilverpark@gmail.com>
2024-08-12 16:07:27 +0900

    feat :: 자원관리 시스템, 생명체 관리시스템

--- third/src/Creature.java
+++ third/src/Creature.java
@@ -4,31 +4,45 @@ import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Creature {
-    private Map<String, String> creatures = new HashMap<>();
+public class Creature implements Management {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private Map<String, String> creatures = new HashMap<>();
 
-    void addCreature(String creatureName) throws IOException {
-        System.out.println("생명체의 이름: "+creatureName);
-        System.out.print("생명체의 정보: ");
-        String creatureInformation = br.readLine();
-        creatures.put(creatureName,creatureInformation);
-    }
-
-    void printCreature(String creatureName){
-        if (creatures.containsKey(creatureName)){
-            System.out.println();
-            System.out.println("-------------------------");
-            System.out.println();
-            System.out.println(" 생명체 정보");
-            System.out.println("\t- 생명체 이름: "+creatureName);
-            System.out.println("\t- 정보: "+creatures.get(creatureName));
-            System.out.println();
-            System.out.println("-------------------------");
+    //새로운 자원을 생성하는 메소드
+    @Override
+    public void createElement(String elementName) throws IOException{
+        if (creatures.containsKey(elementName)){
+            System.out.println(elementName+"라는 이름의 생명체 정보가 이미 존재합니다.");
         }
         else{
-            System.out.println();
-            System.out.println(creatureName+"라는 이름의 생명체 정보를 조회할 수 없습니다.");
+            System.out.println("생명체의 이름: " + elementName);
+            System.out.print("생명체의 설명: ");
+
+            String elementInformation = br.readLine();
+
+            creatures.put(elementName, elementInformation);
+        }
+    }
+    //자원을 삭제하는 메소드
+    @Override
+    public void deleteElement(String elementName) {
+        if (creatures.containsKey(elementName)){
+            creatures.remove(elementName);
+        }
+        else {
+            System.out.println(elementName+"라는 이름의 생명체 정보를 조회할 수 없습니다.");
+        }
+    }
+    //자원 정보를 출력하는 메소드
+    @Override
+    public void printElements(String elementName) {
+        if (creatures.containsKey(elementName)){
+            System.out.println("생명체 정보");
+            System.out.println("\t생명체의 이름: "+elementName);
+            System.out.println("\t생명체의 설명: "+creatures.get(elementName));
+        }
+        else {
+            System.out.println(elementName+"라는 이름의 생명체 정보를 조회할 수 없습니다.");
         }
     }
 }
--- third/src/Resource.java
+++ third/src/Resource.java
@@ -3,105 +3,154 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
-public class Resource {
-    private Map<String, Integer> resources = new HashMap<>();
+public class Resource implements Management{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private Map<String, Integer> resources = new HashMap<>();
+    private String elementErrorMessage;
 
-    public Resource(){
-    }
-
-    void setResource(String resourceName){
-        System.out.println("\n현재 자원의 양: "+resources.get(resourceName));
-    }
-
-    void putResource(String resourceName) throws IOException {
-        System.out.println();
-        System.out.println("자원이름: "+resourceName);
-        System.out.print("자원의 양: ");
-        String resourceQuantity = br.readLine();
-        int resourceQuantityToSet=Integer.parseInt(resourceQuantity);
-
-        if (resourceQuantityToSet<=0){
-            System.out.println();
-            System.out.println("양수로 입력해주세요");
-            putResource(resourceName);
-        }
-
-        else{
-            resources.put(resourceName, resourceQuantityToSet);
-        }
-    }
-
-    void addResourceQuantity(String resourceName) throws IOException {
-        if (resources.containsKey(resourceName)){
-            setResource(resourceName);
-            System.out.print("더할 자원의 양: ");
-            String resourceQuantity = br.readLine();
-            int resourceQuantityToAdd = Integer.parseInt(resourceQuantity);
-
-            if (resourceQuantityToAdd<=0){
-                System.out.println();
-                System.out.println("양수로 입력해주세요");
-                addResourceQuantity(resourceName);
-            }
-
-            else{
-                resources.replace(resourceName, resources.get(resourceName)+resourceQuantityToAdd);
-            }
+    //새로운 자원을 생성하는 메소드
+    @Override
+    public void createElement(String elementName) {
+        if (resources.containsKey(elementName)){
+            System.out.println(elementName+"라는 이름의 자원이 이미 존재합니다.");
         }
         else{
-            System.out.println();
-            System.out.print(resourceName+"라는 이름의 자원이 존재하지 않습니다.");
-            System.out.println();
+            try {
+                System.out.println("자원의 이름: " + elementName);
+                System.out.print("자원의 양: ");
+
+                String elementQuantityToSet = br.readLine();
+
+                elementErrorMessage = "자원의 양은 양수로 입력 되어야 합니다.";
+                int elementQuantity = validateElement(elementQuantityToSet,elementErrorMessage);
+
+                System.out.println("현재 자원의 양: " + elementQuantity);
+                resources.put(elementName, elementQuantity);
+
+            } catch (Exception e) {
+                handleExceptions(e);
+            }
         }
     }
 
-    void useResourceQuantity(String resourceName) throws IOException {
-        if (resources.containsKey(resourceName)){
-            setResource(resourceName);
-            System.out.print("사용할 자원의 양: ");
-            String resourceQuantity = br.readLine();
-            int resourceQuantityToUse = Integer.parseInt(resourceQuantity);
-
-            if (resources.get(resourceName)-resourceQuantityToUse<=0){
-                System.out.println();
-                System.out.println("사용할 자원의 양이 현재 존재하는 자원의 양보다 많습니다.");
-                System.out.println("다시 입력해주세요.");
-                useResourceQuantity(resourceName);
-            }
-            else if (resourceQuantityToUse<=0) {
-                System.out.println();
-                System.out.println("양수로 입력해주세요");
-                useResourceQuantity(resourceName);
-
-            }
-            else{
-                resources.replace(resourceName, resources.get(resourceName)-Integer.parseInt(resourceQuantity));
-            }
+    //자원을 삭제하는 메소드
+    @Override
+    public void deleteElement(String elementName) {
+        if (resources.containsKey(elementName)){
+            resources.remove(elementName);
         }
-        else{
-            System.out.println();
-            System.out.print(resourceName+"라는 이름의 자원이 존재하지 않습니다.");
-            System.out.println();
+        else {
+            System.out.println(elementName+"라는 이름의 자원은 존재하지 않습니다.");
         }
     }
 
-    void printResourceInformation(String resourceName){
-        if (resources.containsKey(resourceName)){
-            System.out.println();
-            System.out.println("-------------------------");
-            System.out.println();
-            System.out.println(" 자원 정보");
-            System.out.println("\t- 자원 이름: "+resourceName);
-            System.out.println("\t- 자원의 양: "+resources.get(resourceName));
-            System.out.println();
-            System.out.println("-------------------------");
+    //자원 정보를 출력하는 메소드
+    @Override
+    public void printElements(String elementName) {
+        if (resources.containsKey(elementName)){
+            System.out.println("자원 정보");
+            System.out.println("\t자원 이름: "+elementName);
+            System.out.println("\t자원 양: "+resources.get(elementName));
         }
-        else{
-            System.out.println();
-            System.out.println(resourceName+"라는 이름의 자원 정보를 조회할 수 없습니다.");
-            System.out.println();
+        else {
+            System.out.println(elementName+"라는 이름의 자원 정보를 조회할 수 없습니다.");
+        }
+    }
+
+    //자원의 양을 더하는 메소드
+    public void addElementQuantity(String elementName) {
+        if (resources.containsKey(elementName)){
+            try {
+                System.out.println("자원의 이름: " + elementName);
+                System.out.print("추가할 자원의 양: ");
+
+                String elementQuantityToAdd = br.readLine();
+
+                elementErrorMessage = "추가할 자원의 양은 양수로 입력 되어야 합니다.";
+                int elementQuantity = validateElement(elementQuantityToAdd,elementErrorMessage);
+                elementQuantity = validateElement(String.valueOf(elementQuantity+resources.get(elementName)),elementErrorMessage);
+
+                System.out.println("현재 자원의 양: " + elementQuantity);
+                resources.put(elementName, elementQuantity);
+
+            } catch (Exception e) {
+                handleExceptions(e);
+                addElementQuantity(elementName);
+            }
+        }
+        else {
+            System.out.println(elementName+"라는 이름의 자원은 존재하지 않습니다.");
+        }
+    }
+
+    //자원을 사용하는 메소드
+    public void useElementQuantity(String elementName) {
+        if (resources.containsKey(elementName)){
+            try {
+                System.out.println("자원의 이름: " + elementName);
+                System.out.print("사용할 자원의 양: ");
+
+                String elementQuantityToUse = br.readLine();
+
+                elementErrorMessage = "사용할 자원의 양은 양수로 입력 되어야 합니다.";
+                int elementQuantity = validateElement(elementQuantityToUse,elementErrorMessage);
+                elementErrorMessage = "사용할 자원의 양은 존재하는 자원의 양보다 많을 수 없습니다.";
+                elementQuantity = validateElement(String.valueOf(resources.get(elementName)-elementQuantity),elementErrorMessage);
+
+                System.out.println("현재 자원의 양: " + elementQuantity);
+                resources.put(elementName, elementQuantity);
+
+            } catch (Exception e) {
+                handleExceptions(e);
+                useElementQuantity(elementName);
+            }
+        }
+        else {
+            System.out.println(elementName+"라는 이름의 자원은 존재하지 않습니다.");
+        }
+    }
+
+    //확인하는 메소드
+    private int validateElement(String elementQuantity, String elementErrorMessage) {
+
+        //trim().isEmpty() -> 공백이 있는 문자열도 빈문자열도 체크할 수 있다.
+        //숫자는 중간에 비어있으면 안됨
+        if (elementQuantity == null || elementQuantity.trim().isEmpty()) {
+            throw new NoSuchElementException("자원의 양을 입력 해주세요.");
+        }
+
+        int quantity = Integer.parseInt(elementQuantity);
+
+        if (quantity < 0) {
+            throw new IllegalArgumentException(elementErrorMessage);
+        }
+
+        return quantity;
+    }
+
+    // 예외를 처리하는 메소드
+    private void handleExceptions(Exception e) {
+
+        //instanceof 를 안쓰면 오류는 없지만
+        //각 타입별로 catch 를 만들어야한다.
+        if (e instanceof NumberFormatException) {
+            System.out.println("정수를 적었는데 이 메세지가 뜨는 경우 숫자의 공백을 제거해주세요.");
+        }
+
+        //음수인 경우
+        else if (e instanceof IllegalArgumentException || e instanceof NoSuchElementException) {
+            System.out.println(e.getMessage());
+        }
+
+        //입출력 과정에서 발생할 수 있는 오류를 나타내는 예외
+        else if (e instanceof IOException) {
+            System.out.println(e.getMessage());
+        }
+
+        else {
+            System.out.println(e.getMessage());
         }
     }
 }
